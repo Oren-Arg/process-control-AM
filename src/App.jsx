1363a53b87b6fb3baa4f9168fb9344ba01dc80ad
Daniel Elektrov <daniel@Daniels-MacBook-Pro.local>
2022-07-16 12:41:55 +0300

    added layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Charts from "./components/Charts/Charts";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Charts />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
